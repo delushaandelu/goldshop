ef97c4ef3610790783e7a7423553619070554d98
Delushaan <delushaan@outlook.com>
2017-02-04 12:07:44 +0530

    code dignater structer process maded

---
+++ application/views/template/footer.php
@@ -0,0 +1,18 @@
+<!-- /#wrapper -->
+
+<!-- jQuery -->
+<script src="<?php echo base_url() ?>resources/vendor/jquery/jquery.min.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="<?php echo base_url() ?>resources/vendor/bootstrap/js/bootstrap.min.js"></script>
+
+<!-- Metis Menu Plugin JavaScript -->
+<script src="<?php echo base_url() ?>resources/vendor/metisMenu/metisMenu.min.js"></script>
+
+<!-- Morris Charts JavaScript -->
+<script src="<?php echo base_url() ?>resources/vendor/raphael/raphael.min.js"></script>
+<script src="<?php echo base_url() ?>resources/vendor/morrisjs/morris.min.js"></script>
+<script src="<?php echo base_url() ?>resources/data/morris-data.js"></script>
+
+<!-- Custom Theme JavaScript -->
+<script src="<?php echo base_url() ?>resources/dist/js/sb-admin-2.js"></script>
